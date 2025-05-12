c15756f07cdb038285f958f4e8e234825883a0a2
Yeoh Joer <yeoh.joer@outlook.com>
2025-05-12 19:36:04 +0800

    fix: cross-platform build and type errors

--- src/index.ts
+++ src/index.ts
@@ -35,11 +35,11 @@ const configs = {
     name: 'compat/flat/recommended',
     plugins: { compat: plugin },
     ...recommended.flat,
-  } as Linter.FlatConfig,
+  } as Linter.Config,
   recommended: {
     plugins: ["compat"],
     ...recommended.legacy,
-  } as Linter.Config,
+  } as Linter.LegacyConfig,
 };
 plugin.configs = configs;
 
